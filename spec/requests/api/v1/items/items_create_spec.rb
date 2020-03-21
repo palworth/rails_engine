38fce936bb2e307572388a1680fbff53d7cdffc0
Pierce Alworth <pbalworth@gmail.com>
2020-03-21 00:26:58 -0500

    Fixed spec with adding merchant and permited params

--- spec/requests/api/v1/items/items_create_spec.rb
+++ spec/requests/api/v1/items/items_create_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe 'ITEMs api' do
+  before :each do
+    @merchant = create(:merchant)
+  end
   it "can create a post route" do
-    first_item = create (:item)
-    item_info = {:id=>31, :name=>"Toy", :description=>"Cool", :unit_price=>1.5,
-                :created_at=>"2020-03-20T20:41:14.833Z",
-                :updated_at=>"2020-03-20T20:41:14.833Z"}
-    post "/api/v1/items/#{first_item.id}", params: {item: item_info}
-
+    item_params = { :name=>"Toy", :description=>"Cool", :unit_price=>1.5,
+                :merchant_id => @merchant.id}
+    post "/api/v1/items", params: item_params
     item = Item.last
 
+    item = Item.last
    expect(response).to be_successful
-   require "pry"; binding.pry
-   expect(item.name).to eq(item_info[:name])
-   expect(item.description).to eq(item_info[:"description"])
+   expect(item.name).to eq(item_params[:name])
+   expect(item.description).to eq(item_params[:"description"])
   end
 end
