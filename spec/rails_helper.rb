39b2bea1700053675bf258ec6f9a8aab7134ffc4
Pierce Alworth <pbalworth@gmail.com>
2020-03-20 23:57:57 -0500

    Refactor

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,5 +68,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
+  end
 end
